54ffd59345a9e22ff325b24fa130d1c5e0e6f496
scy <3297598513@qq.com>
2021-12-26 21:31:03 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+public class HelloNumbers
+{
+	public static void main(String[] args)
+	{
+		int x=0;
+		while(x<10)
+		{
+			System.out.print(x+" ");
+			x++;
+		}
+		//test
+		System.out.println(5+"10");
+		System.out.println(5+10);
+	}
+}
